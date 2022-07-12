296d0059c46de14df73b173cb2eeb14290b415eb
Mateusz Spiewak <spiewaak9@gmail.com>
2022-07-12 14:45:51 +0200

    set prisma client cleaned homepage

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../db/client";
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const user = await prisma.user.findFirst();
+  res.status(200).json({ name: user?.email });
 }
