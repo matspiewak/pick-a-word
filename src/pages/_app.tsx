28b29280c47ea296b35eb7aa0d9323f1ecb920a3
matspiewak <spiewaak9@gmail.com>
2022-07-22 21:12:36 +0200

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,7 @@ export default withTRPC<AppRouter>({
     const url = process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/api/trpc`
       : 'http://localhost:3000/api/trpc';
-
+    console.log(url)
     return {
       url,
       /**
