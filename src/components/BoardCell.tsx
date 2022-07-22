fd21834209cc6d42cabfa2f5284a4e513b3235e7
matspiewak <spiewaak9@gmail.com>
2022-07-22 20:13:41 +0200

    add selected row check

--- src/components/BoardCell.tsx
+++ src/components/BoardCell.tsx
@@ -1,7 +1,13 @@
-import styles from '@/styles/Home.module.css'
-import { useState } from 'react'
+import styles from "@/styles/Home.module.css";
+import { useState } from "react";
 
-export const BoardCell:React.FC<{value:number}> = ({value}) => {
+export const BoardCell: React.FC<{ value: number }> = ({ value }) => {
+  const selectRow: (rowNumber: number) => boolean = (
+    rowNumber: number
+  ): boolean => {
+    if (value % 15 === rowNumber) return true;
+    return false;
+  };
 
-    return <div className={styles.Cell}>{value}</div>
-}
+  return <div className={`${styles.Cell} ${selectRow(0) ? styles.highlight : ''}`}>{value}</div>;
+};
